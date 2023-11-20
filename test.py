5d4be389ef2df81a75be8649405cf7feec010494
kaesinol <keiplyer@gmail.com>
2023-11-20 22:15:41 +0800

    :art::fire: remove dead code & formatting

--- test.py
+++ test.py
@@ -10,8 +10,7 @@ def get_tweet():
     set_work_directory(Path(__file__).absolute().parent)
     config.load({"proxy": {"http": "socks5://127.0.0.1:7890", "https": "socks5://127.0.0.1:7890"}, "max_threads": 2,
                  "header": {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
-                                          "like Gecko) "}
-                    , "inject_js": "/media/Data/Project/twitter_user_tweet_crawler/script.js",
+                                          "like Gecko) "}, "inject_js": "/media/Data/Project/twitter_user_tweet_crawler/script.js",
                  "save": "/media/Data/Project/twitter_user_tweet_crawler/output/", }
                 )
     from twitter_user_tweet_crawler.tweet import Tweet
