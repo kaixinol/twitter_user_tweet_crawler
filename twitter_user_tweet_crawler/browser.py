e2ae32640d97408b4212d1d19f4a061273ae62ce
开普以尔 <keiplyer@gmail.com>
2024-04-04 17:42:23 +0800

    :pencil2:Update browser.py

--- twitter_user_tweet_crawler/browser.py
+++ twitter_user_tweet_crawler/browser.py
@@ -9,7 +9,7 @@ def get_browser(headless: bool = False) -> WebDriver:
     chrome_options.add_argument('--disable-remote-fonts')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=1200x600"')
+    chrome_options.add_argument('--window-size=1200x600')
     if headless:
         chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(options=chrome_options)
